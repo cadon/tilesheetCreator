d830e9567d15c2db72c0f243be63a1a90c7d964d
cadaei <cadaei@web.de>
2017-01-24 18:06:59 +0100

    Add project files.

---
+++ tileCreator/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace tileCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelProduct = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonSaveTilesheet = new System.Windows.Forms.Button();
+            this.buttonCSS2Clipboard = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numericUpDownSize = new System.Windows.Forms.NumericUpDown();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.labelnewSize = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Folder with Icons (or Drag\'nDrop)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No Folder selected";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create TileSheet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(62, 19);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // labelProduct
+            // 
+            this.labelProduct.AutoSize = true;
+            this.labelProduct.Location = new System.Drawing.Point(114, 21);
+            this.labelProduct.Name = "labelProduct";
+            this.labelProduct.Size = new System.Drawing.Size(13, 13);
+            this.labelProduct.TabIndex = 4;
+            this.labelProduct.Text = "×";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.labelProduct);
+            this.groupBox1.Location = new System.Drawing.Point(12, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 46);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "icons-arrangement";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "icon-";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 145);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(203, 48);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "css-class-prefix";
+            // 
+            // buttonSaveTilesheet
+            // 
+            this.buttonSaveTilesheet.Location = new System.Drawing.Point(12, 273);
+            this.buttonSaveTilesheet.Name = "buttonSaveTilesheet";
+            this.buttonSaveTilesheet.Size = new System.Drawing.Size(96, 38);
+            this.buttonSaveTilesheet.TabIndex = 9;
+            this.buttonSaveTilesheet.Text = "Save TileSheet";
+            this.buttonSaveTilesheet.UseVisualStyleBackColor = true;
+            this.buttonSaveTilesheet.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonCSS2Clipboard
+            // 
+            this.buttonCSS2Clipboard.Location = new System.Drawing.Point(114, 273);
+            this.buttonCSS2Clipboard.Name = "buttonCSS2Clipboard";
+            this.buttonCSS2Clipboard.Size = new System.Drawing.Size(101, 38);
+            this.buttonCSS2Clipboard.TabIndex = 10;
+            this.buttonCSS2Clipboard.Text = "Copy css-code to Clipboard";
+            this.buttonCSS2Clipboard.UseVisualStyleBackColor = true;
+            this.buttonCSS2Clipboard.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 247);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(351, 20);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Columns:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(231, 296);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(126, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "TileSheetCreator@github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.labelnewSize);
+            this.groupBox3.Controls.Add(this.labelSize);
+            this.groupBox3.Controls.Add(this.numericUpDownSize);
+            this.groupBox3.Location = new System.Drawing.Point(221, 93);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(142, 100);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resize Icons";
+            // 
+            // numericUpDownSize
+            // 
+            this.numericUpDownSize.Location = new System.Drawing.Point(67, 45);
+            this.numericUpDownSize.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.Name = "numericUpDownSize";
+            this.numericUpDownSize.Size = new System.Drawing.Size(66, 20);
+            this.numericUpDownSize.TabIndex = 0;
+            this.numericUpDownSize.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.ValueChanged += new System.EventHandler(this.numericUpDownSize_ValueChanged);
+            // 
+            // labelSize
+            // 
+            this.labelSize.AutoSize = true;
+            this.labelSize.Location = new System.Drawing.Point(6, 16);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(70, 26);
+            this.labelSize.TabIndex = 1;
+            this.labelSize.Text = "Source-Icons\r\n0 × 0";
+            // 
+            // labelnewSize
+            // 
+            this.labelnewSize.AutoSize = true;
+            this.labelnewSize.Location = new System.Drawing.Point(6, 68);
+            this.labelnewSize.Name = "labelnewSize";
+            this.labelnewSize.Size = new System.Drawing.Size(52, 26);
+            this.labelnewSize.TabIndex = 2;
+            this.labelnewSize.Text = "New Size\r\n0 × 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "new width";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 318);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonCSS2Clipboard);
+            this.Controls.Add(this.buttonSaveTilesheet);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TileSheetCreator";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelProduct;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonSaveTilesheet;
+        private System.Windows.Forms.Button buttonCSS2Clipboard;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.NumericUpDown numericUpDownSize;
+        private System.Windows.Forms.Label labelnewSize;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ tileCreator/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tileCreator
+{
+    public partial class Form1 : Form
+    {
+        private string path, saveFile;
+        private string css, cssPre1, cssPre2;
+        private string[] files;
+        private Bitmap tileSheet;
+        private int width, height;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                setPath(fbd.SelectedPath);
+            }
+        }
+
+        private void setPath(string newPath)
+        {
+            path = newPath;
+            files = Directory.GetFiles(path, "*.png", SearchOption.TopDirectoryOnly);
+            label1.Text = "Selected Path: " + path + ", " + files.Length + " files.";
+            progressBar1.Value = 0;
+            numericUpDown1.Value = (decimal)Math.Ceiling(Math.Sqrt(files.Length));
+
+            // get size from first image
+            System.Drawing.Image img = System.Drawing.Image.FromFile(files[0]);
+            width = img.Width;
+            height = img.Height;
+            img.Dispose();
+            labelSize.Text = "Size of source-icons:\n" + width + " × " + height;
+            numericUpDownSize.Value = width;
+            saveFile = "tilesheet.png";
+
+            buttonSaveTilesheet.Enabled = false;
+            buttonCSS2Clipboard.Enabled = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int rows = (int)Math.Ceiling(files.Length / numericUpDown1.Value);
+            labelProduct.Text = "× " + rows + " = " + (numericUpDown1.Value * rows);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            files = Directory.GetFiles(path, "*.png", SearchOption.TopDirectoryOnly);
+            if (files.Length > 0)
+            {
+                int cols = (int)numericUpDown1.Value;
+                int rows = (int)Math.Ceiling(files.Length / numericUpDown1.Value);
+
+                int nWidth = (int)numericUpDownSize.Value;
+                int nHeight = (int)Math.Round(numericUpDownSize.Value * height / width);
+
+                if ((nWidth * cols > 20000 || nHeight * rows > 20000) && MessageBox.Show("The resulting image will be very large (" + (nWidth * cols) + " × " + (nHeight * rows) + " px, proceed?", "Large image", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes) { return; }
+
+                progressBar1.Maximum = files.Length;
+                progressBar1.Value = 0;
+                Cursor = Cursors.WaitCursor;
+
+                string cssPrefix = textBox1.Text;
+
+                Regex r = new Regex(@"\-+$");
+                string cssBaseClass = r.Replace(cssPrefix, "");
+                cssPre1 = "/* tilesheet */\n." + ((cssBaseClass.Length == 0) ? "icon" : cssBaseClass) + " {\n    background-image: url('images/";
+                cssPre2 = "');\n    width: " + nWidth + "px;\n    height: " + nHeight + "px;\n}";
+                css = "";
+
+                tileSheet = new Bitmap(nWidth * cols, nHeight * rows);
+
+                Graphics g = Graphics.FromImage(tileSheet);
+                if (nWidth != width)
+                {
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                }
+
+                int i = 0;
+                r = new Regex(@"[^a-z0-9]+");
+
+                foreach (string file in files)
+                {
+                    int x = (i % cols) * nWidth;
+                    int y = (i / cols) * nHeight;
+
+                    Image icon;
+                    // avoiding to lock the loaded icon-file
+                    using (var bmpTemp = new Bitmap(file))
+                    {
+                        icon = new Bitmap(bmpTemp);
+                    }
+                    g.DrawImage(icon, x, y, nWidth, nHeight);
+                    string className = r.Replace(Path.GetFileNameWithoutExtension(file).ToLower(), "-");
+                    css += "\n." + cssPrefix + className + " { background-position: " + (-x) + (x == 0 ? "" : "px") + " " + (-y) + (y == 0 ? "" : "px") + "; }";
+                    i++;
+                    progressBar1.Value = i;
+                }
+                buttonSaveTilesheet.Enabled = true;
+                buttonCSS2Clipboard.Enabled = true;
+                Cursor = Cursors.Default;
+            }
+
+            else { MessageBox.Show("No files in the selected folder", "No Files", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tileSheet != null && tileSheet.Width > 0)
+            {
+                SaveFileDialog fd = new SaveFileDialog();
+                fd.Filter = "*.png|*.png|*.jpg|*.jpg";
+                fd.Title = "Save the Tilesheet";
+                fd.ShowDialog();
+
+                // If the file name is not an empty string open it for saving.
+                if (fd.FileName != "")
+                {
+                    saveFile = Path.GetFileName(fd.FileName);
+                    System.IO.FileStream fs = (System.IO.FileStream)fd.OpenFile();
+                    switch (fd.FilterIndex)
+                    {
+                        case 1:
+                            tileSheet.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+
+                        case 2:
+                            tileSheet.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                    }
+
+                    fs.Close();
+                }
+            }
+            else { MessageBox.Show("No Tilesheet created, please choose a folder with files you want to merge into one tilesheed first", "No Tilesheet", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(cssPre1 + saveFile + cssPre2 + css);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://github.com/"); // TODO
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            DragDropEffects effect = DragDropEffects.None;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+                if (Directory.Exists(path))
+                    effect = DragDropEffects.Copy;
+            }
+            e.Effect = effect;
+        }
+
+        private void numericUpDownSize_ValueChanged(object sender, EventArgs e)
+        {
+            labelnewSize.Text = "New size of icons:\n" + numericUpDownSize.Value + " × " + Math.Round(numericUpDownSize.Value * height / width);
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            setPath(((string[])e.Data.GetData(DataFormats.FileDrop))[0]);
+        }
+
+    }
+}
